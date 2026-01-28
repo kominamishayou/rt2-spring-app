569d4f735caffba5126716f31932b13b6cfe42c4
edu <edu@WIN-BC69AF3BT5E>
2026-01-28 14:18:53 +0900

    最終調整

---
+++ spring_crud_exc5_10_kominami/src/main/java/jp/co/sss/crud/err/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package jp.co.sss.crud.err;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GlobalExceptionHandler implements ErrorController {
+
+		@RequestMapping("/error")
+		public String handleException() {
+			return "redirect:/";
+		}
+}
